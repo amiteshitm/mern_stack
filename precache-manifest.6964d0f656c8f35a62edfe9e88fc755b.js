86fb23e252cc58894c03482e4570ffa5b0cdace6
amiteshitm <amiteshitm2011@gmail.com>
2020-05-21 17:42:49 +0530

    Updates

---
+++ precache-manifest.6964d0f656c8f35a62edfe9e88fc755b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f54bcea9a8c8eefa95bd4b2c8e401ec",
+    "url": "/mern_stack/index.html"
+  },
+  {
+    "revision": "36f0998f416030a832a9",
+    "url": "/mern_stack/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a49df4dc101ba38454e",
+    "url": "/mern_stack/static/js/2.74252361.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mern_stack/static/js/2.74252361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f0998f416030a832a9",
+    "url": "/mern_stack/static/js/main.9cde0ebf.chunk.js"
+  },
+  {
+    "revision": "b89ce93fea176089e6b3",
+    "url": "/mern_stack/static/js/runtime-main.67f13ca3.js"
+  }
+]);
